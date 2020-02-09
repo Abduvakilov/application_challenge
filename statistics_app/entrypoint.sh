86541f89751ef0367e085c82715691eb9d995272
Dilshod <abd.dilshod@gmail.com>
2020-02-09 14:15:32 +0500

    small changes

--- statistics_app/entrypoint.sh
+++ statistics_app/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-set -e
+# Exit immediately if a command exits with a non-zero status.
+set -ex
 
+# Get environment variables from .env file
 export $(egrep -v '^#' .env | xargs)
 
 rake db:create
-rake db:migrate
