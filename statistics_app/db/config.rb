1bd9ad4c8fd437bf191bca32d7522c9a459cb6f5
Dilshod <abd.dilshod@gmail.com>
2020-02-09 14:14:38 +0500

    database config

---
+++ statistics_app/db/config.rb
@@ -0,0 +1,42 @@
+require 'active_record'
+require 'pathname'
+
+ActiveSupport::Deprecation.silenced = true
+
+class Config 
+    
+  class << self; attr_accessor :db_config, :root end
+  @db_config = nil
+  @root = Pathname.new(File.expand_path('../..', __FILE__))
+
+  def self.root
+    @root
+  end
+
+  def self.environment
+    ENV['ENVIRONMENT'] ||= 'production'
+  end
+
+  def self.load_database_yaml
+    return unless File.exists?(root.join('db/database.yml').to_s)
+    require 'yaml'
+    require 'erb'
+
+    yaml = File.read(root.join('db/database.yml').to_s)
+    erb = ERB.new(yaml)
+    YAML::load(erb.result)
+  end
+
+  def self.db_config
+    @db_config ||= load_database_yaml
+  end
+
+  def self.current_db_config
+    db_config[environment]
+  end
+
+  def self.current_database_name
+    current_db_config['database']
+  end
+
+end
